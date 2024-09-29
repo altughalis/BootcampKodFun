8824a7cfc1946751b8588dc979e3b6dee836f47f
altughalis <altughalis@gmail.com>
2024-09-29 22:12:58 +0300

    fonksiyonlar

--- BootcampKodFun/ortak/yaz.js
+++ BootcampKodFun/ortak/yaz.js
@@ -2,6 +2,7 @@ document.body.style.background = "black";
 document.body.style.color = "white";
 document.body.style.fontSize = "3em";
 document.body.style.fontFamily = '"Courier New", monospace';
+
 function yaz(html = "") {
   document.body.innerHTML += html + "<br>";
 }
